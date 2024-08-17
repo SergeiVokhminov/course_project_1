2ce4828002bd722b152144e8a751f0a93535bab8
sergei_vokhminov <rreaper@list.ru>
2024-08-18 00:28:42 +0300

    обновлен модуль test_services.py

--- tests/test_services.py
+++ tests/test_services.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List
 import pytest
 import json
 
-from src.services import simple_search
+from src.services import simple_search, filter_numbers
 
 
 def test_simple_search():
@@ -19,3 +19,19 @@ def test_simple_search():
     )
 
     assert simple_search(transactions, search) == expected_result
+
+
+def test_phone_number():
+    transactions = [
+        {"Категория": "Мобильная связь", "Описание": "Тинькофф Мобайл +7 995 555-55-55"},
+        {"Описание": "Оплата счета", "Категория": "Утилиты"},
+        {"Категория": "Мобильная связь", "Описание": "Я МТС +7 921 11-22-33"},
+    ]
+    expected_result = json.dumps(
+        [
+            {"Категория": "Мобильная связь", "Описание": "Тинькофф Мобайл +7 995 555-55-55"},
+            {"Категория": "Мобильная связь", "Описание": "Я МТС +7 921 11-22-33"}
+        ],
+        ensure_ascii=False,
+    )
+    assert filter_numbers(transactions) == expected_result
