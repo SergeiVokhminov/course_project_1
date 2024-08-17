29f66d32f01e8514ab99d487b8a08df09aa5642a
sergei_vokhminov <rreaper@list.ru>
2024-08-17 23:40:06 +0300

    обновлен модуль utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,45 @@
 import json
 import os
+import logging
 import requests
+import pandas as pd
 
 from datetime import datetime
 from collections import defaultdict
 from typing import Any, List, Dict
 from dotenv import load_dotenv
-from config import PATH_TO_FILE, setup_logger
-from read_files import read_file
 
-logger = setup_logger("utils", "logs/utils.log")
+#  Путь до XLSX-файла
+PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "..", "data", "operations.xlsx")
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+rel_file_path = os.path.join(current_dir, "../logs/utils.log")
+abs_file_path = os.path.abspath(rel_file_path)
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler(abs_file_path, "w", encoding="UTF-8")
+file_formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 load_dotenv()
 apilayer_token = os.getenv("API_KEY")
 alphavantage_token = os.getenv("API_KEY_STOCK")
+
+
+def read_file(filename: str = "data/operations.xlsx"):
+    """
+    Функция для считывания финансовых операций из Excel.
+    :param filename: Принимает путь к файлу Excel в качестве аргумента.
+    :return: Выводит список словарей с транзакциями.
+    """
+
+    try:
+        pd_excel = pd.read_excel(filename)
+        return pd_excel.to_dict(orient="records")
+    except Exception as ex:
+        print(f"Неверный формат файла. Ошибка {ex}")
 
 
 def prints_a_greeting(date_str: str):
